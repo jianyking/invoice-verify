d7085f398a1480718a3de9bc40d013e411298d6f
paouke <admin@ninthcode.com>
2018-04-11 14:27:55 +0800

    添加跨域支持

---
+++ src/main/java/com/flamemaster/platform/infra/microservice/config/CORSConfig.java
@@ -0,0 +1,29 @@
+package com.flamemaster.platform.infra.microservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * CORS configuration
+ */
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("PUT", "DELETE","GET","POST")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+
+}
